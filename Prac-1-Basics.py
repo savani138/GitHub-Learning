8ac7ee31ba6830d66cf49d6dd076d697b446b345
savani138 <savani138@gmail.com>
2021-12-27 11:22:23 +0530

    Add files via upload

---
+++ Prac-1-Basics.py
@@ -0,0 +1,18 @@
+firstvar = "Hello"
+print(firstvar)
+# inuser = input("Enter Your Name")
+# print(firstvar)
+# print(inuser)
+
+
+#This is Demo Program 
+ msg = "Hello...."
+ print (msg)
+# Nameofuser=input("Pl. Enter Your Name:")
+# print(msg)
+# print(Nameofuser)
+""" a = 2
+b = int(input("Enter the Number:"))
+z = a + b
+print(z) """
+
---
+++ Prac-2-DataTypes.py
@@ -0,0 +1,47 @@
+""" a = 23
+print(type(a))
+print(a)
+b = 23.67
+print(type(b))
+print(b) """
+""" c = "String Data"
+#print(type(c))
+print(c[11]) """
+""" 
+d = False
+print(type(d)) """
+
+#Sequ
+# DIFFERENCE BETWEEN
+# LIST - Square braces, Mix, Duplicate, Ordered, Mutable
+# TUPLE - Rounded braces, Mix, Order, Dupl, Unmutable
+
+# list1 = ["Computer", "Printer", "TV", "Camera", 89, 30.8, 89]
+# print(list1)
+#print(list1[0:2:1])  #start:end:step
+# list1[0] = "PC"
+# print(list1)
+
+# tuple1 = ("Computer", "Printer", "TV", "Camera", 89, 30.8)
+# tuple1[0] = "PC" # This is not possible
+# print(tuple1)
+
+# SET - The set keyword, Unodered, Unique, Inedex-NotP
+# DICTIONARY - curly braces: made up of key-value pairs
+# Key - Unique, Accessusing Keys
+
+""" set1 = set(["Computer", "Printer", "TV", "Camera", 89, 30.8])
+print(set1) """
+
+# Dict Key:"Value"
+""" dict1 = { 
+    1: "Monday", 
+    2: "Tuesday", 
+    3: "Wednesday"
+    }  
+print(dict1[1]) """
+
+userinfoq = {
+    'Name': "Testing", 
+    'Age' : 9}
+print(userinfoq['Age'])
